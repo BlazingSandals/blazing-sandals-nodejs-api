9ac088ea9b9bb8361202b8323b6b650ceb8368f4
jamesellis <jim@jre3.com>
2024-08-12 13:52:36 -0600

    rename refactor

--- api/routes.ts
+++ api/routes.ts
@@ -1,8 +1,9 @@
 
 import express from 'express';
-import { defaultRouter as techstacksRouter } from './techstacks';
-import { defaultRouter as workhistoryRouter } from './workhistory'
+import { defaultRouter as techstacksRouter } from './tech-stacks';
+import { defaultRouter as workhistoryRouter } from './work-history'
 export const routes = express.Router();
 
+//TODO: add search endpoint
 routes.use("/api/tech-stacks", techstacksRouter);
 routes.use("/api/work-histories", workhistoryRouter);
--- index.ts
+++ index.ts
@@ -1,6 +1,4 @@
-/**
- * Required External Modules
- */
+
 import * as dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
@@ -11,9 +9,6 @@ import { notFoundHandler } from "./middleware/not-found.middlware";
 
 dotenv.config();
 
-/**
- * App Variables
- */
 if (!process.env.PORT) {
     process.exit(1);
 }
@@ -22,19 +17,14 @@ const PORT: number = parseInt(process.env.PORT as string, 10);
 
 const app = express();
 
-/**
- *  App Configuration
- */
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use(errorHandler);
 app.use(notFoundHandler);
+//TODO: add linting
 
-/**
- * Server Activation
- */
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
 });
